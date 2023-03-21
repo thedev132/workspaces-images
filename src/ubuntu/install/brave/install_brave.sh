acb2845d521b8e9ec8699fc8413b55609e971a85
Mohamad M <54006217+thedev132@users.noreply.github.com>
2023-03-21 16:58:12 -0700

    Update install_brave.sh

--- src/ubuntu/install/brave/install_brave.sh
+++ src/ubuntu/install/brave/install_brave.sh
@@ -8,7 +8,7 @@ apt install -y  apt-transport-https curl
 
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc |  apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" |  tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb https://brave-browser-apt-release.s3.brave.com/ stable main" |  tee /etc/apt/sources.list.d/brave-browser-release.list
 
 apt update
 
