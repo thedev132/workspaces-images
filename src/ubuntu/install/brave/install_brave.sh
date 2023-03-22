b6ee61587cb82724299bccb466f18ce06ebee179
Mohamad M <54006217+thedev132@users.noreply.github.com>
2023-03-21 17:01:41 -0700

    Change brave to arm64

--- src/ubuntu/install/brave/install_brave.sh
+++ src/ubuntu/install/brave/install_brave.sh
@@ -6,9 +6,8 @@ CHROME_ARGS="--password-store=basic --no-sandbox --ignore-gpu-blocklist --user-d
 apt-get update
 apt install -y  apt-transport-https curl
 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc |  apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-
-echo "deb https://brave-browser-apt-release.s3.brave.com/ stable main" |  tee /etc/apt/sources.list.d/brave-browser-release.list
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=arm64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 apt update
 
