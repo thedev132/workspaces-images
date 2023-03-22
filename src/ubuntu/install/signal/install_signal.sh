545cfd358cfb61dbeb683751656a8946afe07afb
Mohamad M <54006217+thedev132@users.noreply.github.com>
2023-03-21 17:32:53 -0700

    Update install_signal.sh

--- src/ubuntu/install/signal/install_signal.sh
+++ src/ubuntu/install/signal/install_signal.sh
@@ -2,16 +2,11 @@
 set -ex
 ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
 
-if [ "${ARCH}" == "arm64" ] ; then
-    echo "Signal for arm64 currently not supported, skipping install"
-    exit 0
-fi
+
 
 
 # Signal only releases its desktop app under the xenial release, however it is compatible with all versions of Debian and Ubuntu that we support.
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | apt-key add -
-echo "deb [arch=${ARCH}] https://updates.signal.org/desktop/apt xenial main" |  tee -a /etc/apt/sources.list.d/signal-xenial.list
-apt-get update
+wget https://github.com/dennisameling/Signal-Desktop/releases/download/v6.2.0/signal-desktop-unofficial_6.2.0_arm64.deb
 
 apt-get install -y signal-desktop
 cp /usr/share/applications/signal-desktop.desktop $HOME/Desktop/
